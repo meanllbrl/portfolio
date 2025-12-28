d31ad8d8d80a6dff11ba5ea93b7cfc6ba82418c9
Mehmet Nuraydın <meanllbrl@gmail.com>
2025-12-28 11:19:51 +0300

    fixes applied

--- apps/website/components/Navigation.tsx
+++ apps/website/components/Navigation.tsx
@@ -77,7 +77,7 @@ export function Navigation() {
           <Link
             href="/"
             className="text-lg font-heading font-bold hover:text-primary transition-colors"
-            onClick={(e) => { handleHomeClick(e); setIsMenuOpen(false); }}
+            onClick={(e: MouseEvent<HTMLAnchorElement>) => { handleHomeClick(e); setIsMenuOpen(false); }}
           >
             {t('home')}
           </Link>
